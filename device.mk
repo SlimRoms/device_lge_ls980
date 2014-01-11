d8538f33676bdc0d69ccec6baaf8c03c7cbaa369
arcardinal <andrewrcardinal@gmail.com>
2014-01-11 01:09:45 -0600

    Update device.mk
    Change-Id: Ibefc0a4c60db1f90ff9ef89be9ce34f06ee3f01c

--- device.mk
+++ device.mk
@@ -20,11 +20,18 @@ $(call inherit-product, device/lge/g2-common/g2.mk)
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
 PRODUCT_PROPERTY_OVERRIDES += \
-	telephony.lteOnCdmaDevice=1 \
-	ro.telephony.default_network=10 \
-	ro.cdma.home.operator.numeric=311480 \
-	ro.cdma.home.operator.alpha=Verizon \
-	ro.cdma.homesystem=64,65,76,77,78,79,80,81,82,83
+        ro.cdma.home.operator.numeric=310120 \
+        ro.cdma.home.operator.alpha=Sprint \
+        telephony.lteOnCdmaDevice=1 \
+        telephony.lte.cdma.device=1 \
+        ro.telephony.default_network=8 \
+        ro.ril.def.preferred.network=8 \
+        ril.subscription.types=NV,RUIM \
+        ro.cdma.subscribe_on_ruim_ready=true \
+        persist.radio.no_wait_for_card=1 \
+        keyguard.no_require_sim=true \
+        telephony.sms.pseudo_multipart=1 \
+        DEVICE_PROVISIONED=1
 
 PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/android.hardware.telephony.cdma.xml:system/etc/permissions/android.hardware.telephony.cdma.xml
